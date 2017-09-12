6c3cf002f85be263a3be1e4958462cf4bb6b4ce7
petrero <petrero@gmail.com>
2017-09-12 12:49:30 +0300

    Copy code from branch 4

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+  entry: './web/assets/js/RepLogApp.js',
+  output: {
+    path: path.resolve(__dirname, 'web', 'build'),
+    filename: 'rep_log.js'
+  }
+}
