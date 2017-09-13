f67c13076609a0e61f5b464ec9af1f163e546b9a
petrero <petrero@gmail.com>
2017-09-13 12:56:14 +0300

    Copy code from branch 17

---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const path = require('path');
+const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: {
+    rep_log: './assets/js/rep_log.js',
+    login: './assets/js/login.js',
+    layout: './assets/js/layout.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'web', 'build'),
+    filename: '[name].js',
+    publicPath: '/build/' 
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|ico|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name]-[hash:6].[ext]'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name]-[hash:6].[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      jQuery: 'jquery',
+      $: 'jquery'
+    }),
+    new CopyWebpackPlugin([
+      //copies to {output}/static
+      {from: './assets/static', to: 'static'}
+    ]),
+  ]
+}
