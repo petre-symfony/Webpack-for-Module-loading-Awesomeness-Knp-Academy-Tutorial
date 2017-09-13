65a73c57a2dbb102db1aaa1e513762703ef78192
petrero <petrero@gmail.com>
2017-09-13 08:50:25 +0300

    babel-polyfill library will give us the Promise polyfill and actually, all Polyfills. Use it inside of layout.js - so that we get the polyfill on every page: require babel-polyfill. I'll add a little comment above this.
    And of course, when we refresh, everything is happy! And it would even be happy if I were using an older browser. Of course, I would be less happy using an older browser... but that's unrelated.
    
    Anyways, let's move on to something amazing: requiring CSS from JavaScript!

--- web/assets/js/layout.js
+++ web/assets/js/layout.js
@@ -2,6 +2,9 @@
 const $ = require('jquery');
 require('bootstrap');
 
+// include this on every page to make sure Promise exists
+require('babel-polyfill');
+
 
 $(document).ready(function() {
   $('[data-toggle="tooltip"]').tooltip();
