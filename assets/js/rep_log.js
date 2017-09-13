7b867251db8e947c761435ad236e58ab7341a2b7
petrero <petrero@gmail.com>
2017-09-13 18:21:00 +0300

    Copy code from branch 23

---
+++ assets/js/rep_log.js
@@ -0,0 +1,11 @@
+const $ = require('jquery');
+require('bootstrap-sass');
+const RepLogApp = require('./Components/RepLogApp');
+
+global.$ = $;
+
+$(document).ready(function() {
+  var $wrapper = $('.js-rep-log-table');
+  var repLogApp = new RepLogApp($wrapper, $wrapper.data('rep-logs'));
+});
+
