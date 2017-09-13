d722fc22f5be473560d65701b23b4a1bb62bfeed
petrero <petrero@gmail.com>
2017-09-13 08:56:49 +0300

    Copy code from branch 11

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    rep_log: './web/assets/js/rep_log.js',
+    login: './web/assets/js/login.js',
+    layout: './web/assets/js/layout.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'web', 'build'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      jQuery: 'jquery',
+      $: 'jquery'
+    })
+  ]
+}
